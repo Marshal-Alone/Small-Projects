01620c3b4c25bb02f98453f9912f846ea97483b2
Marshal-Alone <marshal3869@gmail.com>
2025-01-03 23:56:23 +0530

    Currency Convertor -> Added swap btn

--- Small Projects/0005 Currency-Convertor-API/app.js
+++ Small Projects/0005 Currency-Convertor-API/app.js
@@ -1,102 +1,85 @@
-const dropdowns = document.querySelectorAll('.dropdown select');
-const btn = document.querySelector('button');
-base_url = 'https://v6.exchangerate-api.com/v6/4cdf7c6f57f1058e2ee551b4/latest'
-let msg = document.querySelector('.msg');
-
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("button");
+base_url = "https://v6.exchangerate-api.com/v6/4cdf7c6f57f1058e2ee551b4/latest";
+let msg = document.querySelector(".msg");
 
 for (let select of dropdowns) {
+	for (curr_code in countryList) {
+		let newOption = document.createElement("option");
+		newOption.innerText = curr_code;
+		newOption.value = curr_code;
 
-    for (curr_code in countryList) {
-        let newOption = document.createElement("option");
-        newOption.innerText = curr_code;
-        newOption.value = curr_code;
+		if (select.name === "from" && curr_code === "USD") {
+			newOption.selected = "selected";
+		} else if (select.name === "to" && curr_code === "INR") {
+			newOption.selected = "selected";
+		}
 
-        if (select.name === 'from' && curr_code === "USD") {
-            newOption.selected = 'selected';
-        } else if (select.name === 'to' && curr_code === 'INR') {
-            newOption.selected = 'selected';
-        }
-
-        select.append(newOption);
-
-    };
-    select.addEventListener('change', (event) => {
-        updateFlag(event.target);
-        console.log(event.target);
-        console.log(event.target.value);
-    })
-
+		select.append(newOption);
+	}
+	select.addEventListener("change", (event) => {
+		updateFlag(event.target);
+		console.log(event.target);
+		console.log(event.target.value);
+	});
 }
 
 const updateFlag = (element) => {
-    let curr_code = element.value;
-    let countruCode = countryList[curr_code];
-    new_src = `https://flagsapi.com/${countruCode}/flat/64.png`
+	//element -> whose flag we want to update
+	let curr_code = element.value;
+	let countruCode = countryList[curr_code];
+	new_src = `https://flagsapi.com/${countruCode}/flat/64.png`;
 
-    let image = element.parentElement.querySelector("img");
-    image.src = new_src;
-}
+	let image = element.parentElement.querySelector("img");
+	image.src = new_src;
+};
 
-const getExchangeRate = async()=> {
-    let amount = document.querySelector('.amount input').value;
+const getExchangeRate = async () => {
+	let amount = document.querySelector(".amount input").value;
 
-    if (amount === '' || amount <= 0) {
-        alert("Enter valid amount")
-    }
+	if (amount === "" || amount <= 0) {
+		alert("Enter valid amount");
+	}
 
-    let from = document.querySelector('#from').value;
-    let to = document.querySelector('#to').value;
-    const url = base_url + `/${from}`
+	let from = document.querySelector("#from").value;
+	let to = document.querySelector("#to").value;
+	const url = base_url + `/${from}`;
 
-    let response = await fetch(url);
-    let data = await response.json();
-    let result = data.conversion_rates[to];
+	let response = await fetch(url);
+	let data = await response.json();
+	let result = data.conversion_rates[to];
 
-    let finalAmount = result * amount;
-    msg.innerText=`${amount} ${from} = ${finalAmount} ${to}`
-}
+	let finalAmount = result * amount;
+	msg.innerText = `${amount} ${from} = ${finalAmount} ${to}`;
+};
 
-
-btn.addEventListener("click",  (event) => {
-    event.preventDefault();
-    getExchangeRate();
-
-})
-
+btn.addEventListener("click", (event) => {
+	event.preventDefault();
+	getExchangeRate();
+});
 
 window.addEventListener("load", () => {
+	getExchangeRate();
+});
+
+//adding swap button
+const swap_btn = document.querySelector("#swap");
+swap_btn.addEventListener("click", () => {
+	//get currrent values
+	let from_value = document.querySelector("#from").value;
+	let to_value = document.querySelector("#to").value;
+
+	//swap values
+	let from = document.querySelector("#from");
+	from.value = to_value;
+
+	let to = document.querySelector("#to");
+	to.value = from_value;
+
+	//update flag
+	updateFlag(from);
+    updateFlag(to);
+    
+    //update exchange rate
     getExchangeRate();
-})
-
-
-
-
-
-
-
-
-
-
-
-
-// const api = '4cdf7c6f57f1058e2ee551b4';
-
-
-// const btn = document.querySelector('#btn');;
-
-// const getCurr = async () => {
-//     let from = document.querySelector('#from').value;
-//     let to = document.querySelector('#to').value;
-//     const url = `https://v6.exchangerate-api.com/v6/${api}/latest/${from}`
-
-//     let response = await fetch(url);
-//     let data = await response.json();
-
-//     console.log(data.conversion_rates[to]);
-
-// }
-
-// btn.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     getCurr();
-// });
+});
